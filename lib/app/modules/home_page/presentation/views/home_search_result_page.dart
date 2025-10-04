7c4ab49738841338e1a778cec9dc000f9d5464d6
sajid <sajidrec@gmail.com>
2025-10-04 14:06:41 +0600

    - home search page initial design

--- lib/app/modules/home_page/presentation/views/home_search_result_page.dart
+++ lib/app/modules/home_page/presentation/views/home_search_result_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:nsn/app/modules/home_page/presentation/controllers/home_search_result_page_controller.dart';
 import 'package:nsn/app/modules/home_page/presentation/widgets/home_search_text_field_widget.dart';
 
